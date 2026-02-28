96e28bea2d596375ac2984981c0e0fd9c10ecc53
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2026-02-28 19:52:36 +0700

    solve day1

--- day1/main.c
+++ day1/main.c
@@ -2,8 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 
+void part2() {
+	int numbers[10240];
+	char word[5];
+	int sz;
+	memset(word, 0, sizeof word);
+	memset(numbers, 0, sizeof numbers);
+	int x = 0;
+	while (1) {
+		if (scanf("%s", word) == EOF) break;;
+		const char *numb = (word + 1);
+		int val = atoi(numb);
+		if (*word == 'R') {
+			numbers[x] = val;
+		} else {
+			numbers[x] = -val;
+		}
+		x += 1;
+	}
 
-int main() {
+	int res = 50;
+	int count = 0;
+	for (int i = 0; i < x; i++) {
+		int r = (numbers[i] % 100);
+		count += abs(numbers[i] / 100);
+		int nxt = ((res + r) % 100 + 100) % 100;
+		if (res != 0 && ((res + r) >= 100 || (res + r) <= 0)) {
+			count += 1;
+		}
+		res = nxt;
+		printf("curr: %d\n", res);
+	}
+	printf("res: %d", count);
+}
+
+void part1() {
 	int numbers[10240];
 	char word[5];
 	int sz;
@@ -33,4 +66,10 @@ int main() {
 		if (res == 0) count++;
 	}
 	printf("res: %d", count);
+}
+
+
+int main() {
+	// part1();
+	part2();
 }
